58cb6cd1fce2c6a73445ab80abf6442c385345f1
Robert Wolf <robert.wolf@wooga.com>
2017-04-25 12:10:45 +0200

    updatedstream publishing

--- lib/epiphan_pearl/parameter_sets/stream_publishing.rb
+++ lib/epiphan_pearl/parameter_sets/stream_publishing.rb
@@ -6,6 +6,23 @@ module EpiphanPearl
         :display_value   => "type",
         :possible_values => [*0..8],
         :value_type      => EpiphanPearl::Types::Integer
+      },
+      {
+        :key             => "rtsp_url",
+        :display_value   => "url",
+      },
+      {
+        :key             => "rtsp_transport",
+        :display_value   => "transport",
+        :possible_values => ["tcp", "udp", ""]
+      },
+      {
+        :key             => "rtmp_username",
+        :display_value   => "username",
+      },
+      {
+        :key             => "rtmp_password",
+        :display_value   => "password",
       }
     ]
   end
