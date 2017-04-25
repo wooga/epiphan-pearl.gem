a1cd263909911501fd2ebf1d75a1722ebe443cb9
Robert Wolf <robert.wolf@wooga.com>
2017-04-25 12:30:33 +0200

    updated streaming settings

--- lib/epiphan_pearl/parameter_sets/stream_publishing.rb
+++ lib/epiphan_pearl/parameter_sets/stream_publishing.rb
@@ -2,18 +2,35 @@ module EpiphanPearl
   class StreamPublishing < ParameterSet
     register_parameters [
       {
+        :key             => "publish_enabled",
+        :display_value   => "publish_enabled",
+        :possible_values => ["on", "off"]
+      },
+      {
         :key             => "publish_type",
         :display_value   => "type",
         :possible_values => [*0..8],
         :value_type      => EpiphanPearl::Types::Integer
       },
       {
+        :key             => "livestream_channel",
+        :display_value   => "livestream_channel",
+      },
+      {
         :key             => "rtsp_url",
-        :display_value   => "url",
+        :display_value   => "rtsp_url",
+      },
+      {
+        :key             => "rtmp_url",
+        :display_value   => "rtmp_url",
+      },
+      {
+        :key             => "rtmp_stream",
+        :display_value   => "rtmp_stream",
       },
       {
         :key             => "rtsp_transport",
-        :display_value   => "transport",
+        :display_value   => "rtsp_transport",
         :possible_values => ["tcp", "udp", ""]
       },
       {
@@ -23,7 +40,7 @@ module EpiphanPearl
       {
         :key             => "rtmp_password",
         :display_value   => "password",
-      }
+      },
     ]
   end
 end
